f668f3a7a3b6f7f6cfa4ae3ec0bf96198fbeb488
Meet Thakkar <meet@Mac.lan>
2026-05-03 13:59:26 +0530

    fix port for render

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.post('/submit', (req, res) => {
     });
 });
 
-app.listen(12345, () => {
-    console.log("Server running on port http://localhost:12345");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
